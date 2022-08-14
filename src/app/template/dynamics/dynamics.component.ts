fdade5099f5e7ed8cc9e7dd7d092a11950f61243
Judit Santos <judit.sant@gmail.com>
2022-08-14 09:53:34 +0200

    template dynamic

--- src/app/template/dynamics/dynamics.component.ts
+++ src/app/template/dynamics/dynamics.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+
+interface Favorite {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-dynamics',
@@ -6,11 +16,35 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DynamicsComponent implements OnInit {
+export class DynamicsComponent {
 
-  constructor() { }
+  newGame: string = '';
 
-  ngOnInit(): void {
+  person: Person = {
+    name: 'John Doe',
+    favorites: [
+      {id: 1, name: 'Elden Ring'},
+      {id: 2, name: 'Stardew Valley'}
+    ]
+    
+  }
+
+  addGame() {
+    const favGame: Favorite = {
+      id: this.person.favorites.length + 1,
+      name: this.newGame
+    }
+
+    this.person.favorites.push({...favGame});
+    this.newGame = '';
+  }
+
+  deleteGame(index: number) {
+    this.person.favorites.splice(index, 1);
+  }
+
+  save() {
+    console.log('Saved form');
   }
 
 }
