5fb3eee334b9bcf73857eac80829ae342c7a4ab2
Judit Santos <judit.sant@gmail.com>
2022-08-14 19:16:00 +0200

    reactive form: dynamic

--- src/app/reactive/dynamics/dynamics.component.ts
+++ src/app/reactive/dynamics/dynamics.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
@@ -6,11 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DynamicsComponent implements OnInit {
+export class DynamicsComponent {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    name: [ , [Validators.required, Validators.minLength(3)]],
+    favorites: this.fb.array( [
+      ['Stardew Valley'],
+      ['Elden Ring']
+    ], Validators.required )
+  });
 
-  ngOnInit(): void {
+  newFavorite: FormControl = this.fb.control('', Validators.required);
+
+  get favoritesArr() {
+    return this.myForm.get('favorites') as FormArray;
+  }
+
+  constructor( private fb: FormBuilder ) { }
+
+  invalidField( field: string ) {
+    return this.myForm.controls[field].errors && 
+           this.myForm.controls[field].touched;
+  }
+
+  addFavorite() {
+    if (this.newFavorite.invalid) {
+      return;
+    }
+
+    //Passes added game into the array form
+    this.favoritesArr.push(new FormControl(this.newFavorite.value, Validators.required));
+
+    this.newFavorite.reset();
+  }
+
+  deleteGame(index: number) {
+    this.favoritesArr.removeAt(index)
+  }
+
+  save() {
+
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value)
   }
 
 }
